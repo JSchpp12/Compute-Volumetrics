b33eeca4bae70d9cb92b8cbfa020c6f61d8d6b55
Jacob Schaupp <jacob.schaupp@gmail.com>
2023-12-26 15:45:33 -0500

    Add more submodules

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # About
 This is a template for applications utilizing Starlight. 
+
+# Setup 
+
+1. build tbb
+2. build openvdb
---
+++ Setup.py
@@ -0,0 +1,30 @@
+#entrypoint for setup 
+
+#builds openVDB for use
+
+import os
+import subprocess
+import platform
+
+if __name__ == "__main__":
+    cmake_compiler = None
+    cmake_command = None
+
+    if platform.system() == "Windows":
+        cmake_command = "cmake.exe"
+    else:
+        cmake_command = "cmake"
+    
+    print()
+    if platform.release() == "10":
+        cmake_compiler = "Visual Studio 11 Win64"
+
+    if cmake_compiler is None: 
+        print("This is not a supported platform for automated builds of depdencies.")
+        exit()
+    
+    cmake_build_dir = os.path.join(os.getcwd(), "libs")
+
+    cmake_command = [cmake_command, "-G", cmake_compiler, cmake_build_dir]
+
+    subprocess.run(cmake_command)
