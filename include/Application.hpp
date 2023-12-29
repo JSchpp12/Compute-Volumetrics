011ee6c5e1371e17c4988c51842726f8d0a1d5af
Jacob Schaupp <jacob.schaupp@gmail.com>
2023-12-28 19:43:31 -0500

    Switch to .bat file

--- include/Application.hpp
+++ include/Application.hpp
@@ -20,6 +20,8 @@
 #include <glm/gtx/transform.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
+#include <openvdb/openvdb.h>
+
 #include <string> 
 #include <memory> 
 
---
+++ prep.bat
@@ -0,0 +1,32 @@
+SET mypath=%~dp0
+SET wpath=%mypath:~0,-1%
+
+IF not exist "%wpath%/prep_work/tbb" mkdir "%wpath%/prep_work/tbb"
+IF not exist "%wpath%/prep_work/blosc" mkdir "%wpath%/prep_work/blosc"
+IF not exist "%wpath%/prep_work/zlib" mkdir "%wpath%/prep_work/zlib"
+IF not exist "%wpath%/prep_work/openVDB" mkdir "%wpath%/prep_work/openVDB"
+
+CD "%wpath%/prep_work/tbb"
+
+cmake -G "Visual Studio 17 2022" -A x64 -DCMAKE_INSTALL_PREFIX=%wpath%/libs/tbb -DTBB_TEST=OFF %wpath%/extern/tbb
+cmake --build . --config DEBUG
+cmake -DCOMPONENT=devel -DBUILD_TYPE=DEBUG -P cmake_install.cmake
+
+cmake --build . --config relwithdebinfo
+cmake -DCOMPONENT=devel -DBUILD_TYPE=relwithdebinfo -P cmake_install.cmake
+
+CD "%wpath%/prep_work/blosc"
+
+cmake -G "Visual Studio 17 2022" -A x64 -DCMAKE_INSTALL_PREFIX=%wpath%/libs/blosc %wpath%/extern/blosc
+cmake --build . --config DEBUG --target INSTALL
+cmake --build . --config relwithdebinfo --target INSTALL
+
+cd "%wpath%/prep_work/zlib"
+
+cmake -G "Visual Studio 17 2022" -A x64 -DCMAKE_INSTALL_PREFIX=%wpath%/libs/zlib %wpath%/extern/zlib
+cmake --build . --target install
+
+cd "%wpath%/prep_work/openVDB"
+
+cmake -G "Visual Studio 17 2022" -A x64 -DCMAKE_INSTALL_PREFIX=%wpath%/libs/openVDB -DTBB_ROOT="%wpath%/libs/tbb" -DBlosc_ROOT=%wpath%/libs/blosc -DZLIB_ROOT=%wpath%/libs/zlib %wpath%/extern/openVDB
+cmake --build . --target install
