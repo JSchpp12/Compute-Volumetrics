34c66589c1923764e286b47e98fc074d3c7167c9
Jacob Schaupp <jacob.schaupp@gmail.com>
2023-12-30 18:41:50 -0500

    Cleanup

--- prep.bat
+++ prep.bat
@@ -4,13 +4,13 @@ SET wpath=%mypath:~0,-1%
 IF not exist "%wpath%/prep_work/openVDB" mkdir "%wpath%/prep_work/openVDB"
 IF not exist "%wpath%/vsproj" mkdir "%wpath%/vsproj"
 
-@REM cd "%wpath%/prep_work/openVDB"
+cd "%wpath%/prep_work/openVDB"
 
-@REM cmake %wpath%/extern/openVDB -G "Visual Studio 17 2022" -A x64 -DCMAKE_TOOLCHAIN_FILE="C:\Users\js400421\vcpkg\scripts\buildsystems\vcpkg.cmake" -DCMAKE_INSTALL_PREFIX=%wpath%/libs/openVDB/Debug -DUSE_EXPLICIT_INSTANTIATIONS=OFF -DOPENVDB_CORE_STATIC=OFF -DCMAKE_CXX_STANDARD=20
-@REM cmake --build . --parallel 6 --config Debug --target install
-@REM cmake %wpath%/extern/openVDB -G "Visual Studio 17 2022" -A x64 -DCMAKE_TOOLCHAIN_FILE="C:\Users\js400421\vcpkg\scripts\buildsystems\vcpkg.cmake" -DCMAKE_INSTALL_PREFIX=%wpath%/libs/openVDB/Release -DUSE_EXPLICIT_INSTANTIATIONS=OFF -DOPENVDB_CORE_STATIC=OFF -DCMAKE_CXX_STANDARD=20
-@REM cmake --build . --parallel 6 --config Release --target install
+cmake %wpath%/extern/openVDB -G "Visual Studio 17 2022" -A x64 -DCMAKE_TOOLCHAIN_FILE=<TOOL_PATH> -DCMAKE_INSTALL_PREFIX=%wpath%/libs/openVDB/Debug -DUSE_EXPLICIT_INSTANTIATIONS=OFF -DOPENVDB_CORE_STATIC=OFF -DCMAKE_CXX_STANDARD=20
+cmake --build . --parallel 6 --config Debug --target install
+cmake %wpath%/extern/openVDB -G "Visual Studio 17 2022" -A x64 -DCMAKE_TOOLCHAIN_FILE=<TOOL_PATH> -DCMAKE_INSTALL_PREFIX=%wpath%/libs/openVDB/Release -DUSE_EXPLICIT_INSTANTIATIONS=OFF -DOPENVDB_CORE_STATIC=OFF -DCMAKE_CXX_STANDARD=20
+cmake --build . --parallel 6 --config Release --target install
 
 cd "%wpath%/vsproj"
 
-cmake -G "Visual Studio 17 2022" -A x64 -DCMAKE_TOOLCHAIN_FILE="C:\Users\js400421\vcpkg\scripts\buildsystems\vcpkg.cmake" -DCMAKE_CXX_STANDARD=20 ..
+cmake -G "Visual Studio 17 2022" -A x64 -DCMAKE_TOOLCHAIN_FILE=<TOOL_PATH> -DCMAKE_CXX_STANDARD=20 ..
