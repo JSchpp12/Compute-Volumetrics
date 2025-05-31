738c06bb9b28f0181bc7ca54659f7a90bc5c8c4c
Jacob Schaupp <jacob.schaupp@gmail.com>
2025-05-31 07:47:09 -0400

    Update windows build scripts

--- build_release.bat
+++ build_release.bat
@@ -1,14 +1,32 @@
+SETLOCAL
+
+REM Get script path
 SET mypath=%~dp0
 SET wpath=%mypath:~0,-1%
+SET appBuilderDir=%wpath%\extern\StarlightAppBuilder
 
-if not exist "%wpath%/build" mkdir "%wpath%/build"
+REM Ensure build directory exists
+if not exist "%wpath%\build" mkdir "%wpath%\build"
 
-call "%wpath%/extern/vcpkg/bootstrap-vcpkg.bat"
+REM Bootstrap vcpkg
+call "%wpath%\extern\vcpkg\bootstrap-vcpkg.bat"
 
-start "" /wait "%wpath%/extern/vcpkg/vcpkg.exe" "integrate" "install" 
+REM Integrate vcpkg
+"%wpath%\extern\vcpkg\vcpkg.exe" integrate install
 
-cd "%wpath%/build"
+REM Go to build directory
+cd /d "%wpath%\build"
 
-cmake -DCMAKE_TOOLCHAIN_FILE="%wpath%/extern/vcpkg/scripts/buildsystems/vcpkg.cmake" -DCMAKE_BUILD_TYPE=Release ..
+REM Check if AppBuilder is initialized
+if exist "%appBuilderDir%" (
+    echo StarlightAppBuilder Already Initialized
+) else (
+    echo Initializing StarlightAppBuilder Project && git submodule init && git submodule update && cd /d "%appBuilderDir%" && git submodule init && git submodule update && call "%appBuilderDir%\init.bat"
+)
 
-cmake --build . -j 6
+REM Configure and build
+cd /d "%wpath%\build"
+cmake -DCMAKE_TOOLCHAIN_FILE="%wpath%\extern\vcpkg\scripts\buildsystems\vcpkg.cmake" -DCMAKE_BUILD_TYPE=Release ..
+cmake --build . -- -j6
+
+ENDLOCAL
