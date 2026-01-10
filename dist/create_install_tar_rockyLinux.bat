cbaf7a7b0f288213cae468046e70e3b52031a02e
Jacob Schaupp <jacob.schaupp@gmail.com>
2026-01-10 10:05:05 -0500

    Windows bat file for building install

---
+++ dist/create_install_tar_rockyLinux.bat
@@ -0,0 +1,28 @@
+
+@echo off
+setlocal enabledelayedexpansion
+
+REM Resolve the directory that this script resides in
+set "SCRIPT_DIR=%~dp0"
+
+REM Normalize to a fully-qualified path
+for %%I in ("%SCRIPT_DIR%.") do set "SCRIPT_DIR=%%~fI"
+
+REM Go TWO directories up from script location
+for %%I in ("%SCRIPT_DIR%..\..") do set "PARENT_DIR=%%~fI"
+
+echo Parent directory: "%PARENT_DIR%"
+
+REM Build Docker image
+pushd deps
+docker build -f rockyLinux_dockerfile -t starlight:rockyLinux .
+popd
+
+REM Run container
+docker run -it ^
+  -v "%PARENT_DIR%:/app" ^
+  -w /app ^
+  starlight:rockyLinux ^
+  /bin/bash -lc "./dist/deps/scripts/build_install.sh"
+
+endlocal
