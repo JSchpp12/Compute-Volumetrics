d508aae5e0092bdbc85f9da1f227a0b0f0db9b73
Jacob Schaupp <jacob.schaupp@gmail.com>
2026-04-18 12:26:17 -0400

    Add optionals for pre and post

--- include/renderer/FullPassVolumeCommands.hpp
+++ include/renderer/FullPassVolumeCommands.hpp
@@ -1,6 +1,10 @@
 #pragma once
 
+#include "renderer/PostMemoryBarrierContributor.hpp"
+#include "renderer/PreMemoryBarrierContributor.hpp"
 #include "renderer/VolumeComputeCommands.hpp"
+
+#include <optional>
 
 class VolumeRenderer;
 
@@ -8,10 +12,26 @@ namespace renderer
 {
 class FullPassVolumeCommands
 {
+    std::optional<PreMemoryBarrierContributor> m_preMemCmds{std::nullopt};
+    std::optional<PostMemoryBarrierContributor> m_postMemCmds{std::nullopt};
     VolumeComputeCommands m_mainCmds;
 
   public:
-    explicit FullPassVolumeCommands(VolumeComputeCommands mainCmds) : m_mainCmds(std::move(mainCmds))
+    explicit FullPassVolumeCommands(VolumeComputeCommands mainCmds)
+        : m_preMemCmds{std::nullopt}, m_postMemCmds{std::nullopt}, m_mainCmds(std::move(mainCmds))
+    {
+    }
+    FullPassVolumeCommands(VolumeComputeCommands mainCmds, PreMemoryBarrierContributor preMemCmds)
+        : m_preMemCmds(std::move(preMemCmds)), m_postMemCmds{std::nullopt}, m_mainCmds(std::move(mainCmds))
+    {
+    }
+    FullPassVolumeCommands(VolumeComputeCommands mainCmds, PostMemoryBarrierContributor postMemCmds)
+        : m_preMemCmds{std::nullopt}, m_postMemCmds(std::move(postMemCmds)), m_mainCmds(std::move(mainCmds))
+    {
+    }
+    FullPassVolumeCommands(VolumeComputeCommands mainCmds, PreMemoryBarrierContributor preMemCmds,
+                           PostMemoryBarrierContributor postMemCmds)
+        : m_preMemCmds(std::move(preMemCmds)), m_postMemCmds(std::move(postMemCmds)), m_mainCmds(std::move(mainCmds))
     {
     }
 
--- src/renderer/FullPassVolumeCommands.cpp
+++ src/renderer/FullPassVolumeCommands.cpp
@@ -3,14 +3,18 @@
 void renderer::FullPassVolumeCommands::recordPreCommands(vk::CommandBuffer cmdBuffer,
                                                          const star::common::FrameTracker &ft)
 {
+    if (m_preMemCmds.has_value())
+        m_preMemCmds.value().recordPreCommands(cmdBuffer, ft);
 }
 
 void renderer::FullPassVolumeCommands::recordPostCommands(vk::CommandBuffer cmdBuffer,
                                                           const star::common::FrameTracker &ft)
 {
+    if (m_postMemCmds.has_value())
+        m_postMemCmds.value().recordPostCommands(cmdBuffer, ft);
 }
 
 void renderer::FullPassVolumeCommands::recordCommands(vk::CommandBuffer cmdBuffer, const star::common::FrameTracker &ft)
 {
-    m_mainCmds.recordCommands(cmdBuffer, ft); 
+    m_mainCmds.recordCommands(cmdBuffer, ft);
 }
