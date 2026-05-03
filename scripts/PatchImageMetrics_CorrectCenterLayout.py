af4826897ef979c09704a86668cca84e46a6655a
Jacob Schaupp <jacob.schaupp@gmail.com>
2026-05-03 10:53:21 -0400

    Rename old script + add new script to correct terrain_center

---
+++ scripts/PatchImageMetrics_CorrectCenterLayout.py
@@ -0,0 +1,209 @@
+import os
+import argparse
+import json
+from tqdm import tqdm
+from dataclasses import dataclass, asdict
+from typing import Optional
+
+from pathlib import Path
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+@dataclass
+class Vec3:
+    x: float
+    y: float
+    z: float
+
+
+@dataclass
+class Vec2:
+    x: float
+    y: float
+
+@dataclass
+class TerrainShapeFile:
+    center: Vec2
+    view_distance: int
+
+    @classmethod
+    def from_dict(cls, data: dict) -> "TerrainShapeFile":
+        center : Optional[Vec2] = None
+        if "lat" in data["center"]: 
+            center = Vec2(data["center"]["lat"], data["center"]["lon"])
+        else:
+            center = Vec2(**data["center"])
+
+        return cls(
+            center,
+            view_distance=data["view_distance"],
+        )
+
+    @classmethod
+    def from_json(cls, path: Path) -> "TerrainShapeFile":
+        with open(path, "r") as f:
+            data = json.load(f)
+            return cls.from_dict(data)
+
+    def to_json(self) -> str:
+        return json.dumps(asdict(self), indent=4)
+
+
+@dataclass
+class ExpFogInfo:
+    density: float
+
+
+@dataclass
+class HomogenousInfo:
+    maxNumSteps: int
+
+
+@dataclass
+class LinearInfo:
+    farDist: float
+    nearDist: float
+
+@dataclass
+class MarchedInfo:
+    defaultDensity: float
+    densityMultiplier: float
+    lightPropertyDirG: float
+    sigmaAbsorption: float
+    sigmaScattering: float
+    stepSizeDist: float
+    stepSizeDist_light: float
+    cutoffValue : Optional[float] = None
+
+
+@dataclass
+class FogParams:
+    expFogInfo: ExpFogInfo
+    homogenousInfo: HomogenousInfo
+    linearInfo: LinearInfo
+    marchedInfo: MarchedInfo
+
+    @classmethod
+    def from_dict(cls, data: dict) -> "FogParams":
+        marched = None
+        if "cutoffValue" not in data["marchedInfo"]: 
+            data["cutoffValue"] = 0.001
+
+        return cls(
+            expFogInfo=ExpFogInfo(**data["expFogInfo"]),
+            homogenousInfo=HomogenousInfo(**data["homogenousInfo"]),
+            linearInfo=LinearInfo(**data["linearInfo"]),
+            marchedInfo=MarchedInfo(**data["marchedInfo"]),
+        )
+
+
+@dataclass
+class Frame:
+    camera_look_dir: Vec3
+    camera_position: Vec3
+    file_name: str
+    fog_params: FogParams
+    fog_type: str
+    visibility_distance: float
+    terrain_shape: Optional[TerrainShapeFile]
+    terrain_name: Optional[str]
+
+    @classmethod
+    def from_dict(cls, data: dict) -> "Frame":
+        tName: Optional[str] = None
+        if "terrain_name" in data:
+            tName = str(data["terrain_name"])
+
+        tData: Optional[TerrainShapeFile] = None
+        if "terrain_shape" in data:
+            tData = TerrainShapeFile.from_dict(data["terrain_shape"])
+
+        return cls(
+            camera_look_dir=Vec3(**data["camera_look_dir"]),
+            camera_position=Vec3(**data["camera_position"]),
+            file_name=data["file_name"],
+            fog_params=FogParams.from_dict(data["fog_params"]),
+            fog_type=data["fog_type"],
+            visibility_distance=data["visibility_distance"],
+            terrain_shape=tData,
+            terrain_name=tName,
+        )
+
+    @classmethod
+    def from_json(cls, path: Path) -> "Frame":
+        with open(path, "r") as f:
+            return cls.from_dict(json.load(f))
+
+    def to_json(self, path: Path) -> None:
+        with open(path, "w") as f:
+            json.dump(asdict(self), f, indent=4)
+
+
+@dataclass
+class ImageToUpdate:
+    path: Path
+    override_shape_info: Optional[TerrainShapeFile]
+    override_name: Optional[str]
+
+
+def gather_image_metric_files(root_dir: Path) -> list[ImageToUpdate]:
+    """Recursively gather all .json file paths from a directory."""
+    return [
+        ImageToUpdate(Path(os.path.join(dirpath, filename)), None, None)
+        for dirpath, _, filenames in os.walk(root_dir)
+        for filename in filenames
+        if filename.endswith(".json")
+    ]
+
+def create_correct_frame_from_bad_dict(data : dict) -> Frame: 
+    x = data["terrain_shape"]["center_lat"]
+    y = data["terrain_shape"]["center_lon"]
+
+    vis = None
+    if "vis_range" in data["terrain_shape"]:
+        vis = data["terrain_shape"]["vis_range"]
+    else:
+        vis = data["terrain_shape"]["view_distance"]
+
+    terrain_shape : TerrainShapeFile = TerrainShapeFile(center=Vec2(x, y), view_distance=vis)
+    data["terrain_shape"] = asdict(terrain_shape)
+    return Frame.from_dict(data)
+
+def process_image_file(img_data : ImageToUpdate) -> None: 
+    corrected_data = None
+    with open(img_data.path, 'r') as f:
+        corrected_data = create_correct_frame_from_bad_dict(json.load(f))
+    with open(img_data.path, 'w') as f:
+        if corrected_data is not None:
+            json.dump(asdict(corrected_data), f, indent=4)
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="Find all JSON files in a directory.")
+    parser.add_argument(
+        "root_dir", type=str, help="Path to the root directory to search."
+    )
+    return parser.parse_args()
+
+def main(): 
+    """_summary_
+    This script corrects the following layout issue in the provided image metric json files: 
+
+    A previous pass of patches applied "center_lat" and "center_lon" to the center paramter in the metric files.
+    This will correct those to 'x' and 'y' to follow new layout of metric files.
+    """
+    
+    args = parse_args()
+    metric_files : list[ImageToUpdate] = gather_image_metric_files(args.root_dir)
+
+    with tqdm(total=len(metric_files), desc="Processing files", unit="file") as bar:
+        with ThreadPoolExecutor(max_workers=16) as executor:
+            futures = {executor.submit(process_image_file, f): f for f in metric_files}
+            for future in as_completed(futures):
+                try:
+                    future.result()
+                except Exception as e:
+                    print(f"Error processing {futures[future].path}: {e}")
+                finally:
+                    bar.update(1)
+
+if __name__ == "__main__": 
+    main()
